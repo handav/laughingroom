cd9e9c9df639d0a29d4ed93048d243f6d963b5c9
handav <hannahdavis0220@gmail.com>
2018-08-20 15:54:56 -0400

    initial commit

--- separate_by_silence.py
+++ separate_by_silence.py
@@ -2,6 +2,7 @@ import librosa
 import csv
 from pydub import AudioSegment
 from pydub import silence
+import numpy
 
 audio_file = './RellBattle_concatenated/RellBattle_concatenated.wav'
 
@@ -13,7 +14,8 @@ with open('silences.csv', 'r') as csvfile:
         silences.append(float(row[0]))
 
 audio, sampling_rate = librosa.load(audio_file)
-intervals = librosa.effects.split(audio, top_db=26)
+intervals = librosa.effects.split(audio, top_db=40)
+print len(intervals)
 
 print librosa.core.get_duration(audio)
 sound_file = AudioSegment.from_wav(audio_file)
@@ -26,23 +28,29 @@ def interval_to_ms(interval):
     return interval_ms[0]*1000.0
 
 tagged_utterances = []
-timestamps = []
+
+# interval_stats = []
+# for r in range(0, len(intervals)):
+#     interval_stats.append(interval_to_ms(intervals[r][1]) - interval_to_ms(intervals[r][0]))
+#     #tagged_utterances.append([interval_to_ms(intervals[r][0]), interval_to_ms(intervals[r][1]), 'setup'])
+
+# print numpy.mean(interval_stats)
+# print numpy.std(interval_stats)
 
 for r in range(0, len(intervals)):
     tagged_utterances.append([interval_to_ms(intervals[r][0]), interval_to_ms(intervals[r][1]), 'setup'])
 
 silence_counter = 0
-punchlines = []
 for i, t in enumerate(intervals):
     start = librosa.core.frames_to_time(librosa.core.samples_to_frames(t[0]))
     end = librosa.core.frames_to_time(librosa.core.samples_to_frames(t[1]))
     if start > silences[silence_counter]:
-        punchlines.append(intervals[i-1])
         silence_counter = silence_counter + 1
         tagged_utterances[i-1] = [interval_to_ms(intervals[i-1][0]), interval_to_ms(intervals[i-1][1]), 'punchline']
     if i == len(intervals)-1:
         tagged_utterances[i] = [interval_to_ms(intervals[i][0]), interval_to_ms(intervals[i][1]), 'punchline']
 
+#SAVE TAGS AND UTTERANCE FILES
 tag_file = open('RellBattletags.csv', 'w')
 
 #Process each chunk per requirements
